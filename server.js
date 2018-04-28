c3a6900aedff6e9ae2a0b595be10ee5397db7329
unknown <kfamily66@yandex.ru>
2018-04-28 13:39:02 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello express!');
+// });
+
+// app.get('/about', (req, res) => {
+//     res.send({
+//         address: "Lysva",
+//         weather: "cloudy"
+//     })
+// });
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        title: 'Homepage',
+        welcomeMessage: 'Hello there!',
+        year: new Date().getFullYear()
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        packageName: "Hbs",
+        year: new Date().getFullYear()
+    });
+})
+
+app.listen(3000);
